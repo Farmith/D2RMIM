c75392051f00f10badaa8378f9ff2ce2116f6415
Farmith <alaknas@gmail.com>
2021-10-27 23:48:27 +0200

    Re-push of everything

---
+++ MultiInstanceManager/MultiInstanceManager.cs
@@ -0,0 +1,98 @@
+﻿using MultiInstanceManager.Modules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiInstanceManager
+{
+    public partial class MultiInstanceManager : Form
+    {
+        MultiHandler MH;
+        public MultiInstanceManager()
+        {
+            InitializeComponent();
+            addAccountButton.Click += new EventHandler(addAccountButton_Click);
+            launchButton.Click += new EventHandler(launchButton_Click);
+            removeButton.Click += new EventHandler(removeButton_Click);
+            refreshButton.Click += new EventHandler(refreshButton_Click);
+            readmeLink.Click += new EventHandler(readmeLink_Click);
+            MH = new MultiHandler(this,accountList);
+            MH.LoadAccounts();
+        }
+        private void addAccountButton_Click(object sender, System.EventArgs e)
+        {
+            MH.Setup();
+        }
+        private void launchButton_Click(object sender, System.EventArgs e)
+        {
+            if(accountList.CheckedItems.Count != 0)
+            {
+                for(var x = 0; x < accountList.CheckedItems.Count; x++)
+                {
+                    try
+                    {
+                        var checkedItem = accountList.CheckedItems[x].ToString().Split('|')[0].Trim(' ');
+
+                        MH.LaunchWithAccount(checkedItem);
+                    } catch(Exception ex)
+                    {
+                        // Something went terribly wrong.. 
+                    }
+                }
+            }
+        }
+        private void removeButton_Click(object sender, System.EventArgs e)
+        {
+            if (accountList.CheckedItems.Count != 0)
+            {
+                for (var x = 0; x < accountList.CheckedItems.Count; x++)
+                {
+                    try
+                    {
+                        var checkedItem = accountList.CheckedItems[x].ToString().Split('|')[0];
+                        accountList.Items.Remove(accountList.CheckedItems[x]);
+                        File.Delete(checkedItem + ".bin");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Something went terribly wrong.. 
+                    }
+                }
+            }
+        }
+        private void refreshButton_Click(object sender, System.EventArgs e)
+        {
+            MH.ClearDebug();
+            if (accountList.CheckedItems.Count != 0)
+            {
+                for (var x = 0; x < accountList.CheckedItems.Count; x++)
+                {
+                    try
+                    {
+                        var checkedItem = accountList.CheckedItems[x].ToString().Split('|');
+
+                        MH.Setup(checkedItem[0].Trim(' '));
+                    }
+                    catch (Exception ex)
+                    {
+                        // Something went terribly wrong.. 
+                    }
+                }
+            }
+        }
+        private void readmeLink_Click(object sender, System.EventArgs e)
+        {
+            var ePath = Application.ExecutablePath;
+            var path = System.IO.Path.GetDirectoryName(ePath);
+            Process.Start(path + "\\README.txt");
+        }
+    }
+}
